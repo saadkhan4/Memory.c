a8f14b85f1066cfdcb2a7cab0f31db5751310119
saadkhan4 <saadshaheen1327@gmail.com>
2024-11-07 22:44:41 +0500

    learning about memory

---
+++ addresses.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// in this file we're going to see any code we type in computer where does it go in computer's memory//
+
+int main(void)
+{
+    int n = 50;
+    printf("%i\n", n);
+}
