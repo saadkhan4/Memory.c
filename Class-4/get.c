e8b663d6ffc7bb7e653e760e290f833a02b2a68c
saadkhan4 <saadshaheen1327@gmail.com>
2024-11-11 22:26:01 +0500

    practiced

---
+++ Class-4/get.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// in this we are not using cs50 libraries using scanf to get the answer//
+
+int main(void)
+{
+    int n;
+    printf("n: ");
+    scanf("%i", &n);
+    printf("n: %i\n", n);
+    
+
+    
+}
