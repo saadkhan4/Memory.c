e8d3215203d78189470b0a458498f9129a5f879d
saadkhan4 <saadshaheen1327@gmail.com>
2024-11-13 23:08:54 +0500

    created csv file of ages

--- Class-5/ages.c
+++ Class-5/ages.c
@@ -1,18 +1,18 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 #include <string.h>
 
 int main(void)
 {
-    FILE *file = fopen("phonebook.csv" , "a");
+    FILE *file = fopen("ages.csv", "a");
     if (file == NULL)
     {
         return 1;
     }
 
     char *name = get_string("Name: ");
-    char *number = get_string("Number: ");
+    char *age = get_string("Age: ");
 
-    fprintf(file, "%s, %s\n", name, number);
+    fprintf(file, "%s ,%s\n", name, age);
     fclose(file);
 }
